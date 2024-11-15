2fa1cf4689072f7ad2c8f3f09fede145187241b5
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2024-11-14 21:53:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function janela() {
+const sla = $(document).ready(() => {
+        console.log('rodou')
+    
+        let tamanho = innerWidth
+    
+        if(tamanho => 576 && $('section').find('.parametro').hasClass('flex-column-reverse')){
+            $('section').find('.parametro').removeClass('flex-column-reverse')
+        }
+    
+        if(tamanho <= 575){
+            $('section').find('.parametro').addClass('flex-column-reverse')
+    }
+})
+
+function janela() {  //preciso melhorar isso, pq ta uma gambiarra do krl 
+    console.log('rodou')
+    
     let tamanho = innerWidth
 
     if(tamanho => 576 && $('section').find('.parametro').hasClass('flex-column-reverse')){
@@ -7,5 +23,5 @@ function janela() {
 
     if(tamanho <= 575){
         $('section').find('.parametro').addClass('flex-column-reverse')
-    }
+}
 }
