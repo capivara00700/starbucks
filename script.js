f16bbd842f35114623f3a4b1aee76862d2e15e8c
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2024-11-22 18:49:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -65,9 +65,52 @@ function janela() {  //preciso melhorar isso, pq ta uma gambiarra do krl
 
 function btn_rodape(e){
 
-    switch(e){
-        case '1': $('.ul1').toggle('medio')
-                  $('#btn-1').animate({'rotate': '180deg'}, 'medio')                
+    if(e === '1'){
+        $('.ul1').toggle('medio')
 
+        if($('#btn-1').hasClass('rotate')){
+            $('#btn-1').removeClass('rotate')
+            $('#btn-1').animate({'rotate': '180deg'}, 'medio')
+        } else {
+            $('#btn-1 ').animate({'rotate': '0deg'}, 'medio')
+            $('#btn-1').addClass('rotate')
+        }
     }
+
+    if(e === '2'){
+        $('.ul2').toggle('medio')
+
+        if($('#btn-2').hasClass('rotate')){
+            $('#btn-2').removeClass('rotate')
+            $('#btn-2').animate({'rotate': '180deg'}, 'medio')
+        } else {
+            $('#btn-2 ').animate({'rotate': '0deg'}, 'medio')
+            $('#btn-2').addClass('rotate')
+        }
+    }
+
+    if(e === '3'){
+        $('.ul3').toggle('medio')
+
+        if($('#btn-3').hasClass('rotate')){
+            $('#btn-3').removeClass('rotate')
+            $('#btn-3').animate({'rotate': '180deg'}, 'medio')
+        } else {
+            $('#btn-3 ').animate({'rotate': '0deg'}, 'medio')
+            $('#btn-3').addClass('rotate')
+        }
+    }
+
+    if(e === '4'){
+        $('.ul4').toggle('medio')
+
+        if($('#btn-4').hasClass('rotate')){
+            $('#btn-4').removeClass('rotate')
+            $('#btn-4').animate({'rotate': '180deg'}, 'medio')
+        } else {
+            $('#btn-4 ').animate({'rotate': '0deg'}, 'medio')
+            $('#btn-4').addClass('rotate')
+        }
+    }
+
 }
