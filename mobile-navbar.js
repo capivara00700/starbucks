fc7de803e0ff51debd2f9c7f6713301c7cbac871
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2024-11-27 22:57:02 -0300

    Update mobile-navbar.js

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,10 +1,14 @@
-$('#botao-menu-responsivo').on('click', () => {
+$('.hamburguer').on('click', () => {
     if($('.menu-mobile').hasClass('aberto')){
         $('.menu-mobile').animate({'margin-left': '-0px'}, 'medio')
         $('.menu-mobile').removeClass('aberto')
     } else {
-        $('.menu-mobile').animate({'margin-left': '-1000px'}, 'medio')
+        $('.menu-mobile').animate({'margin-left': '-2000px'}, 'medio')
         $('.menu-mobile').addClass('aberto')
     }
 })
 
+//botão
+$('.hamburguer').on('click', () => {
+    $('.nav').toggleClass('active')
+})
