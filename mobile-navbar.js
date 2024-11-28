e3ef366088cf08194fad9f11e0cbccfe724eeb1a
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2024-11-27 22:23:50 -0300

    Update mobile-navbar.js

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,42 +1,10 @@
-class MobileNavbar {
-    constructor(mobileMenu, navbarnav, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navbarnav);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
+$('#botao-menu-responsivo').on('click', () => {
+    if($('.menu-mobile').hasClass('aberto')){
+        $('.menu-mobile').animate({'margin-left': '-0px'}, 'medio')
+        $('.menu-mobile').removeClass('aberto')
+    } else {
+        $('.menu-mobile').animate({'margin-left': '-500px'}, 'medio')
+        $('.menu-mobile').addClass('aberto')
     }
-  
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards 1.5s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
-  }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".navbar-nav",
-    ".navbar-nav li",
-  );
-  mobileNavbar.init();
+})
+
