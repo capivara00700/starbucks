ab6e88ad2f84794b9d6c3ce6db82ec70e19303d3
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2024-12-16 16:27:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,43 @@
-const sla = $(document).ready(() => {
-    
-        let tamanho = innerWidth
-    
-        if(tamanho => 576 && $('section').find('.parametro').hasClass('flex-column-reverse')){
-            $('section').find('.parametro').removeClass('flex-column-reverse')
-        }
-    
-        if(tamanho <= 575){
-            $('section').find('.parametro').addClass('flex-column-reverse')
-    }
-
-        /*--Rodapé--pt1*/
-        if(tamanho => 576){
-            $('.rdp1').find('.row, div').addClass('col')
-        }
-        
-        if(tamanho <= 575){
-            $('.rdp1').find('.row, div').removeClass('col')
-        }
-
-        /*--Rodapé--*/
-        if(tamanho => 576){
-            $('.btnr').show()
-        }
-        
-        if(tamanho <= 575){
-            $('.btnr').hide()
-    } 
-
-})
-
-function janela() {  //preciso melhorar isso, pq ta uma gambiarra do krl 
-    
+function menu() {
     let tamanho = innerWidth
 
-    if(tamanho => 576 && $('section').find('.parametro').hasClass('flex-column-reverse')){
-        $('section').find('.parametro').removeClass('flex-column-reverse')
-    }
+    let menu = $('.row').find('.menu-div')
+    let janela = $('section').find('.parametro')
+    let ul = $('footer, .row').find('ul')
 
-    if(tamanho <= 575){
-        $('section').find('.parametro').addClass('flex-column-reverse')
-    }
+    if(tamanho <= 576){
+        //menu
+        $(menu).addClass('col-sm')
+        $(menu).removeClass('col-7')
+        $(menu).removeClass('col-5')
+        
+        //index janelas
+        $(janela).addClass('flex-column-reverse')
 
-    /*--Rodapé--pt1*/
-    if(tamanho => 576){
-        $('.rdp1').find('.row, div').addClass('col')
-    }
-    
-    if(tamanho <= 575){
-        $('.rdp1').find('.row, div').removeClass('col')
-    }
+        //rodapé
+        $(ul).addClass('d-none')
 
-    
-    /*--Rodapé--*/
-    if(tamanho => 576){
-        $('.btnr').show()
-    }
-    
-    if(tamanho <= 575){
-        $('.btnr').hide()
-    } 
+        let btn = $('.rdp1').find('button')
 
+        $(btn).on('click', e => {
+            console.log($(e.target()).parent())
+        })
+
+
+    } else if(tamanho => 576){
+        //menu
+        $(menu).removeClass('col-sm')
+        $('.row').find('.div1').addClass('col-7')
+        $('.row').find('.div2').addClass('col-5')
+
+      //index janelas
+      $(janela).removeClass('flex-column-reverse')        
+
+        //rodapé
+        $(ul).removeClass('d-none')
+    }
 }
 
-function btn_rodape(e){
-
-    if(e === '1'){
-        $('.ul1').toggle('medio')
-
-        if($('#btn-1').hasClass('rotate')){
-            $('#btn-1').removeClass('rotate')
-            $('#btn-1').animate({'rotate': '180deg'}, 'medio')
-        } else {
-            $('#btn-1 ').animate({'rotate': '0deg'}, 'medio')
-            $('#btn-1').addClass('rotate')
-        }
-    }
-
-    if(e === '2'){
-        $('.ul2').toggle('medio')
-
-        if($('#btn-2').hasClass('rotate')){
-            $('#btn-2').removeClass('rotate')
-            $('#btn-2').animate({'rotate': '180deg'}, 'medio')
-        } else {
-            $('#btn-2 ').animate({'rotate': '0deg'}, 'medio')
-            $('#btn-2').addClass('rotate')
-        }
-    }
-
-    if(e === '3'){
-        $('.ul3').toggle('medio')
-
-        if($('#btn-3').hasClass('rotate')){
-            $('#btn-3').removeClass('rotate')
-            $('#btn-3').animate({'rotate': '180deg'}, 'medio')
-        } else {
-            $('#btn-3 ').animate({'rotate': '0deg'}, 'medio')
-            $('#btn-3').addClass('rotate')
-        }
-    }
-
-    if(e === '4'){
-        $('.ul4').toggle('medio')
-
-        if($('#btn-4').hasClass('rotate')){
-            $('#btn-4').removeClass('rotate')
-            $('#btn-4').animate({'rotate': '180deg'}, 'medio')
-        } else {
-            $('#btn-4 ').animate({'rotate': '0deg'}, 'medio')
-            $('#btn-4').addClass('rotate')
-        }
-    }
-
-}
+$(document).ready(() => {
+    menu()
+})
